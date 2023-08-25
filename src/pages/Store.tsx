6b46a4716c1efcb95719ad254f9ecb53290bcda6
ismeet010 <ismeetchhabda@gmail.com>
2023-08-25 19:58:30 +0530

    adding navbar

---
+++ src/pages/Store.tsx
@@ -0,0 +1,16 @@
+import { Row, Col} from "react-bootstrap"
+import StoreItems from "../data/items.json"
+
+function Store() {
+  return (
+    <>
+    <h1>Store</h1>
+    <Row>
+        <Col></Col>
+    </Row>
+    </>
+    
+  )
+}
+
+export default Store
